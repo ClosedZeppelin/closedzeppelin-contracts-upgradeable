c4f7b12f52d962a77c8e73b0a687cce587f18bda
Amaury Diaz <amauryuh@gmail.com>
2024-10-07 17:22:05 +0200

    placeholder for new contracts.

---
+++ src/utils/MultisigUpgradeable.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+// ClosedZeppelin Contracts (utils/MultisigUpgradeable.sol)
+
+pragma solidity ^0.8.20;
+
+import {Initializable} from "openzeppelin-contracts-upgradeable/contracts/proxy/utils/Initializable.sol";
+
+contract MultisigUpgradeable is Initializable {
+    function __MultisigUpgradeable_init() internal onlyInitializing {}
+}
